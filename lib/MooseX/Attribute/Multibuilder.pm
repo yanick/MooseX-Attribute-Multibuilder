f7070804e107833b832b8f0766a3c2be91c7e0fd
Yanick Champoux <yanick@cpan.org>
2018-01-26 19:00:13 -0500

    first stab

--- lib/MooseX/Attribute/Multibuilder.pm
+++ lib/MooseX/Attribute/Multibuilder.pm
@@ -1,13 +1,83 @@
 package MooseX::Attribute::Multibuilder;
-# ABSTRACT: 
+# ABSTRACT: Have several attributes share the same builder
 
 =head1 SYNOPSIS
 
+    package Foo;
+    use Moose;
+    use MooseX::Attribute::Multibuilder;
+
+    has bar => (
+        traits => [ 'Multibuilder' ],
+        is => 'ro',
+        multibuilder => '_build_them_all'
+    );
+
+    has baz => (
+        traits => [ 'Multibuilder' ],
+        is => 'ro',
+        multibuilder => '_build_them_all'
+    );
+
+    sub _build_them_all {
+        return {
+            bar => 'BAR',
+            baz => 'BAZ' 
+        };
+    }
+
+
+    my $foo = Foo->new;
+
+    print $foo->bar; # BAR
+    print $foo->baz; # BAZ
+
 =head1 DESCRIPTION
+
+Adds a C<multibuilder> option, which is like Moose's C<builder>, but is
+expected to return a hashref of attribute and their default values.
 
 =cut
 
 use strict;
 use warnings;
+
+use Moose::Role;
+
+use List::Util 1.29 qw/ pairmap /;
+
+has multibuilder => (
+    is       => 'ro',
+    required => 1,
+);
+
+before _process_options => sub {
+    my( $class, $name, $options ) = @_;
+
+    my $builder = $options->{multibuilder} or return;
+
+    $options->{default} = sub {
+        my $self = shift;
+        my %values = %{ $self->$builder };
+
+        while( my ( $attr, $val ) = each %values ) {
+            my $obj = $self->meta->get_attribute($attr) or next;
+            $obj->set_value($self,$val)
+                unless $obj->has_value($self);
+        }
+
+        return $values{$name};
+    };
+};
+
+{
+    package Moose::Meta::Attribute::Custom::Trait::Multibuilder;
+
+    sub register_implementation { 'MooseX::Attribute::Multibuilder' }
+
+}
+
+1;
+
 
 1;
